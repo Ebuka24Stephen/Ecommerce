07b822ec45c4f2e30958b558198612511f44c219
ebuka <sebuka559@gmail.com>
2024-11-15 16:43:54 +0100

    added users to installed apps

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    path('users/', include('users.urls')),
     path('orders/', include('orders.urls')),
     path('', include('shop.urls')),
     path('cart', include('cart.urls')),
